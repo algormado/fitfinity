2384cf227236f8d81709f6746214e37e1de75e1e
linda-20 <lindaaron40@gmail.com>
2024-02-14 23:56:53 +0300

    Added firebase that will handle authentication

--- src/Components/Registration/Register.jsx
+++ src/Components/Registration/Register.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import { auth } from '../firebaseConfig';
+import { auth } from './firebaseConfig';
 
 function Register() {
   const [email, setEmail] = useState('');
